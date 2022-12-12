34ab92edda02204224b94bb8809b61fd494c2b39
KatarinaKomad <komad.katarina@gmail.com>
2022-12-12 13:48:43 +0100

    Sitne izmene seme

--- docs/XMLParsers/src/rs/ac/uns/ftn/jaxb/test/A1.java
+++ docs/XMLParsers/src/rs/ac/uns/ftn/jaxb/test/A1.java
@@ -1,17 +1,12 @@
 package rs.ac.uns.ftn.jaxb.test;
 
-import rs.ac.uns.ftn.a1.ZahtevZaAutorskoDelo;
-import rs.ac.uns.ftn.jaxb.example4.Address;
-import rs.ac.uns.ftn.jaxb.example4.AddressBook;
-import rs.ac.uns.ftn.jaxb.example4.Person;
-import rs.ac.uns.ftn.jaxb.util.MyDatatypeConverter;
-import rs.ac.uns.ftn.jaxb.z1.ZahtevZaPriznanjeZiga;
+import java.io.File;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
-import java.io.File;
+
+import rs.ac.uns.ftn.jaxb.a1.ZahtevZaAutorskoDelo;
 
 public class A1 {
     public void test() {
@@ -19,17 +14,17 @@ public class A1 {
             System.out.println("[INFO] Autorsko delo: JAXB XML Schema validation.\n");
 
             // Definiše se JAXB kontekst (putanja do paketa sa JAXB bean-ovima)
-            JAXBContext context = JAXBContext.newInstance("rs.ac.uns.ftn.a1");
+            JAXBContext context = JAXBContext.newInstance("rs.ac.uns.ftn.jaxb.a1");
 
             // Unmarshaller - zadužen za konverziju iz XML-a u objektni model
             Unmarshaller unmarshaller = context.createUnmarshaller();
 
             // Učitavanje XML-a u objektni model
-            ZahtevZaAutorskoDelo zahtev = (ZahtevZaAutorskoDelo) unmarshaller.unmarshal(new File("./data/zig.xml"));
+            ZahtevZaAutorskoDelo zahtev = (ZahtevZaAutorskoDelo) unmarshaller.unmarshal(new File("./data/A1.xml"));
 
             // Ispis sadržaja objekta
             System.out.println("[INFO] Unmarshalled content:");
-            // System.out.println(zahtev);
+            System.out.println(zahtev);
 
         } catch (JAXBException e) {
             e.printStackTrace();
