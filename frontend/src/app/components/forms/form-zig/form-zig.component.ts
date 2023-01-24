da2dd9a18053c515fc6299e5d4383335979de2c6
Nevena Prokic <prokic.sw6.2019@uns.ac.rs>
2023-01-24 22:02:42 +0100

    Dodati prilozi za zahtev za priznanje ziga

--- frontend/src/app/components/forms/form-zig/form-zig.component.ts
+++ frontend/src/app/components/forms/form-zig/form-zig.component.ts
@@ -6,6 +6,7 @@ import {ZigService} from "../../../services/zig/zig.service";
 import {Toastr} from "../../../services/utils/toastr/toastr.service";
 import {FormConverterService} from "../../../services/zig/form-converter/form-converter.service";
 import {XmlTemplateService} from "../../../services/zig/xml-template/xml-template.service";
+import {AD_PrilogType, Z_PrilogType} from "../../../model/model";
 
 @Component({
     selector: 'app-form-zig',
@@ -13,10 +14,24 @@ import {XmlTemplateService} from "../../../services/zig/xml-template/xml-templat
     styleUrls: ['./form-zig.component.scss']
 })
 export class FormZigComponent {
+    primerakZnakaEncoded: any ='';
+    spisakEncoded: any = '';
+    opstiAktEncoded: any ='';
+    pravoPrvenstvaEncoded: any = '';
+    uplataTakseEncoded: any ='';
+    punomocjeEncoded: any = '';
+
+    prilogTypes=Z_PrilogType
 
     zigForm = this.fb.group({});
     form = new FormGroup({
-        pravoPrvenstvaIOsnov: new FormControl('')
+        pravoPrvenstvaIOsnov: new FormControl(''),
+        primerakZnaka: new FormControl(),
+        spisakRobeIUsluga: new FormControl(),
+        opstiAkt: new FormControl(),
+        dokazOPravuPrvenstva: new FormControl(),
+        dokazOUplatiTakse: new FormControl(),
+        punomocje: new FormControl(),
     })
 
     toggle = Array.from({length: 45}, () => true);
@@ -75,5 +90,66 @@ export class FormZigComponent {
 
     openDialogResenje() {
         this.dialog.open(FormResenjeComponent);
+    }
+
+    uploadPrilog(input: HTMLInputElement, prilogType: Z_PrilogType): void{
+        if(input.files && input.files[0]){
+            this.handleFileSelect(input.files[0], prilogType)
+        }
+    }
+
+    private handleFileSelect(file: File, prilogType: Z_PrilogType){
+        const reader = new FileReader();
+        switch (prilogType) {
+            case Z_PrilogType.PRIMERAK_ZNAKA:
+                reader.onload = this._handlePrimerakZnakaReader.bind(this);
+                break;
+            case Z_PrilogType.SPISAK_ROBE_I_USLUGA:
+                reader.onload = this._handleSpisakReader.bind(this);
+                break;
+            case Z_PrilogType.OPSTI_AKT:
+                reader.onload = this._handleOpstiAktReader.bind(this);
+                break;
+            case Z_PrilogType.DOKAZ_O_PRAVU_PRVENSTVA:
+                reader.onload = this._handlePavoPrvenstvaReader.bind(this);
+                break;
+            case Z_PrilogType.DOKAZ_O_UPLATI_TAKSE:
+                reader.onload = this._handleUplataTakseReader.bind(this);
+                break;
+            case Z_PrilogType.PUNOMOCJE:
+                reader.onload = this._handlePunomocjeReader.bind(this);
+                break;
+            default:
+                break;
+        }
+        reader.readAsBinaryString(file);
+    }
+
+    private _handlePrimerakZnakaReader(readerEvt: any) {
+        var binaryString = readerEvt.target.result;
+        this.primerakZnakaEncoded = btoa(binaryString);
+    }
+
+    private _handleSpisakReader(readerEvt: any) {
+        var binaryString = readerEvt.target.result;
+        this.spisakEncoded = btoa(binaryString);
+    }
+    private _handleOpstiAktReader(readerEvt: any) {
+        var binaryString = readerEvt.target.result;
+        this.opstiAktEncoded = btoa(binaryString);
+    }
+
+    private _handlePavoPrvenstvaReader(readerEvt: any) {
+        var binaryString = readerEvt.target.result;
+        this.pravoPrvenstvaEncoded = btoa(binaryString);
+    }
+    private _handleUplataTakseReader(readerEvt: any) {
+        var binaryString = readerEvt.target.result;
+        this.uplataTakseEncoded = btoa(binaryString);
+    }
+
+    private _handlePunomocjeReader(readerEvt: any) {
+        var binaryString = readerEvt.target.result;
+        this.punomocjeEncoded = btoa(binaryString);
     }
 }
